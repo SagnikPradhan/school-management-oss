b026bf47dce51f04c089beb5b57c668c6e48a609
Sagnik Pradhan <markten6321@gmail.com>
2020-11-14 22:42:48 +0530

    ♻️ Refactored form and its components

--- src/web/components/form.tsx
+++ src/web/components/form.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import * as z from "zod"
 import React from "react"
 import { useForm } from "workspace/web/hooks/form"
+import { FormField } from "./form-field"
 
 export type SubmitFn<Form> = ( fields: Record<keyof Form, string> ) => void
 
@@ -10,79 +13,50 @@ export const Form = <F extends string>({
   onSubmit: clientOnSubmit
 }: {
   form: Record<F, {
-    // Anything other than any, such as never or unknown doesn't seem to work
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     schema: z.ZodSchema<any, any>
     type: React.InputHTMLAttributes<HTMLInputElement>["type"]
   }>
   submitText: string
   onSubmit: ( fields: Record<keyof typeof form, string> ) => void
 }): JSX.Element => {
-  const { register, state, error } = useForm()
-  const fields: JSX.Element[] = []
-
-  let index = 0
+  // Extract information for useForm
+  const formWithoutTypes = {} as Record<F, z.ZodSchema<any, any>>
   for ( const name in form ) {
-    const { schema, type } = form[ name ]
-
-    const field = (
-      <div key={index++} className="field">
-        <label htmlFor={name}>{name}</label>
-        <input
-          id={name}
-          className={ state[ name ]?.error ?  "error" : "" } 
-          type={type}
-          onChange={ register( name, schema ) }
-        />
-
-        <style jsx>{`
-          .field {
-            display: flex;
-            flex-direction: column;
-            gap: 0.5em;
-          }
-
-          input {
-            background: white;
-
-            padding: 0.5em;
-
-            color: rgb(35, 35, 35);
-            font-size: 0.85rem;
-            font-family: 'Open Sans', sans-serif;
-
-            border-radius: 5px;
-            border: 1px solid rgba(0, 0, 0, 0.2);
-            box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.1);
-          }
-
-          label {
-            text-transform: capitalize;
-          }
-
-          input.error {
-            border-color: #DE3C4B;
-          }  
-        `}</style>
-      </div>
-    )
-
-    fields.push( field )
+    const { schema } = form[ name ]
+    formWithoutTypes[ name ] = schema
   }
 
+  const { register, state } = useForm( formWithoutTypes )
+
+  // Create the fields
+  let index = 0
+  const fields: JSX.Element[] = []
+  for ( const name in form ) {
+    const { type } = form[ name ]
+    fields.push( 
+      <FormField
+        key={ index++ }
+        error={ !!state.errors[ name ] }
+        name={ name }
+        onChangeEvent={ register( name ) }
+        type={ type }
+      /> 
+    )
+  }
+
+  // On submit event
   const onSubmit = ( event: React.FormEvent<HTMLFormElement> ) => {
     event.preventDefault()
 
     const fields = Object.fromEntries(
       Object
-        .entries( state )
+        .entries( state.values )
         .map(
-          ( [ field, errorAndValue ] ) => {
-            const { value } = errorAndValue || {}
+          ( [ name, value ] ) => {
             if ( typeof value === "undefined" )
               throw new Error( "Found field value undefined" )
             else 
-              return [ field, value ]
+              return [ name, value ]
           }
         )
     )
@@ -94,23 +68,17 @@ export const Form = <F extends string>({
     <form onSubmit={ onSubmit }>
       { fields }
 
-      <div className="errors">
-        {
-          Object
-            .keys( state )
-            .map( ( field, key ) =>  (
-              <span key={ key }>{ state[ field ]?.error }</span>
-            ) )
-        }
-      </div>
+      <div className="errors">{
+        Object
+          .values( state.errors )
+          .map( ( field ) =>  field )
+          .filter( Boolean )
+          .map( ( field, key ) => <span key={ key }>{ field }</span> )
+      }</div>
 
       <button
         type="submit"
-        disabled={
-          error ||
-          state.email?.value === undefined ||
-          state.password?.value === undefined
-        }
+        disabled={ state.isErrored || !state.isDirty }
       >{ submitText }</button>
 
       <style jsx>{`
--- src/web/hooks/form.ts
+++ src/web/hooks/form.ts
@@ -1,47 +1,44 @@
-import { Dispatch, useEffect, useReducer, useState } from "react"
+import { Dispatch, useReducer } from "react"
 import * as z from "zod"
 
 /**
  * useForm hook
  */
-export function useForm<F extends string>(): {
-  error: boolean
+export function useForm<
+  F extends string,
+  A, 
+  B extends z.ZodTypeDef,
+  S extends Record<F, z.ZodSchema<A, B>>
+>( schemas: S ): {
   state: FormState<F>
-  register: ReturnType<typeof registerInputFactory>
+  register: ( name: F ) => ( ev: React.ChangeEvent<HTMLInputElement> ) => void
 } {
-  const [ state, dispatchEvent ] = useReducer( reducer, {})
-  const [ error, setError ] = useState( false )
-
-  useEffect( () => 
-    setError(
-      Object
-        .values( state )
-        .some( ( fieldState ) => typeof fieldState?.error === "string" )
-    ),
-  [ state ] )
-
-  return {
-    error,
-    state,
-    register: registerInputFactory( dispatchEvent )
+  const initialState = { 
+    errors: {}, values: {}, isErrored: false, isDirty: false 
   }
-}
 
-/**
- * Register input paramters
- */
-export interface RegisterInputParams<A, B extends z.ZodTypeDef> {
-  name: string
-  schema: z.ZodSchema<A, B>
+  const [ state, dispatchEvent ] = useReducer( reducer, initialState )
+  return { state, register: registerInputFactory( dispatchEvent, schemas ) }
 }
 
 /**
  * Form state
  */
-export type FormState<F extends string> = Record<F, {
-  error: string | null
-  value: string | undefined
-} | undefined>
+export interface FormState<F extends string> {
+  errors: Record<F, string | undefined>
+  values: Record<F, string | undefined>
+  isErrored: boolean
+  isDirty: boolean
+}
+
+/**
+ * Internal dispatch params
+ */
+interface DispatchParams<F extends string> {
+  event: React.ChangeEvent<HTMLInputElement>
+  name: F
+  schema: z.ZodSchema<never, never>
+}
 
 /**
  * Reducer for our complex state
@@ -52,22 +49,25 @@ export type FormState<F extends string> = Record<F, {
  */
 function reducer<F extends string>(
   state: FormState<F>,
-  [ event, { name, schema } ]: [
-    React.ChangeEvent<HTMLInputElement>,
-    RegisterInputParams<never, never>
-  ]
+  { event, name, schema }: DispatchParams<F>
 ): FormState<F> {
   const value = event.target.value
   const result = schema.safeParse( value )
 
-  if ( result.success ) return { ...state, [name]: { value, error: null } } 
-  else return {
-    ...state,
-    [ name ]: {
-      value: undefined,
-      error: result.error.errors[0].message
-    }
+  const errors = { 
+    ...state.errors,
+    [ name ]: result.success ? undefined : result.error.errors[0].message
   }
+
+  const values = { 
+    ...state.values,
+    [ name ]: result.success ? result.data : undefined
+  }
+
+  const isErrored = Object.values( errors ).some( Boolean )
+  const isDirty = Object.values( values ).every( Boolean )
+
+  return { errors, values, isErrored, isDirty }
 }
 
 /**
@@ -75,20 +75,21 @@ function reducer<F extends string>(
  * 
  * @param dispatchEvent - React dispatch event method
  */
-function registerInputFactory(
-  dispatchEvent: Dispatch<[
-    React.ChangeEvent<HTMLInputElement>,
-    RegisterInputParams<never, never>
-  ]>
+function registerInputFactory<F extends string, A, B extends z.ZodTypeDef>(
+  dispatchEvent: Dispatch<DispatchParams<F>>,
+  schemas: Record<F, z.ZodSchema<A, B>>
 ) {
-  return function registerInput<A, B extends z.ZodTypeDef>(
-    name: string,
-    schema: z.ZodSchema<A, B>
+  return function registerInput(
+    name: F
   ) {
+    const schema = schemas[ name ]
+
     return ( event: React.ChangeEvent<HTMLInputElement> ) => {
-      dispatchEvent(
-        [ event, { name, schema: schema as z.ZodSchema<never, never> } ] 
-      )
+      dispatchEvent({
+        name, 
+        schema: schema as z.ZodSchema<never, never>,
+        event 
+      })
     }
   }
 }
