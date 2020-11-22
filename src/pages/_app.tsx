46cc0c6d58d403cc3a2a696fd3662f46db07c8e5
Sagnik Pradhan <markten6321@gmail.com>
2020-11-22 11:18:29 +0530

    ➕ Added prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app"
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
   body {
