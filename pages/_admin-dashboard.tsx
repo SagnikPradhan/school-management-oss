d442b11ed7a196aa05bea0b3a7e49ad27ce8f0b5
Sagnik Pradhan <markten6321@gmail.com>
2020-12-10 10:17:15 +0530

    ♻️ Refactored with new batching hook

--- pages/_admin-dashboard.tsx
+++ pages/_admin-dashboard.tsx
@@ -1,6 +1,36 @@
-import { SchoolUsers } from "lib/components/school-users";
+import * as z from "zod";
+
 import { SchoolAdmin } from "lib/types/user";
+import { useBatch } from "lib/utility/batch.hook";
+import { Form } from "lib/components/form";
+import { Table } from "lib/components/table";
 
 export default function AdminDashboard() {
-  return <SchoolUsers<SchoolAdmin> role="school-admin" />;
+  const users = useBatch<SchoolAdmin>("users", (users) =>
+    users.where("role", "==", "school-admin")
+  );
+
+  return (
+    <div className="page">
+      <Table
+        headers={{ email: { header: true }, school: {} }}
+        data={users.data.map(({ email, school }) => ({
+          email,
+          school,
+        }))}
+      />
+
+      <Form
+        values={{
+          email: ["", z.string().email(), "email"],
+          school: ["", z.string(), "text"],
+        }}
+        onSubmit={async ({ email, school }) =>
+          users.add(email, { email, school, role: "school-admin" })
+        }
+      />
+
+      <button onClick={users.commitAndReset}>Commit</button>
+    </div>
+  );
 }
